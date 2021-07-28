1485538b6d21af51d4bf7a9e72600dcee0bf779d
junioralcant <junioralcant111@gmail.com>
2021-07-28 16:29:48 -0300

    Cria repositorio de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,17 @@
 import { Router } from 'express';
 
-import { Category } from '../models/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
 
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (req, res) => {
   const { name, description } = req.body;
 
-  const category = new Category();
+  categoriesRepository.create({ name, description });
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
-
-  categories.push(category);
-
-  return res.json({ category });
+  return res.send();
 });
 
 export { categoriesRoutes };
