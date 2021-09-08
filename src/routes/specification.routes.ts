a4f3b09a172b6e8e5d4240fede3bcfe7364be761
junioralcant <junioralcant111@gmail.com>
2021-09-08 15:45:12 -0300

    Separating repositories

--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { SpecificationRepository } from '../modules/cars/repositories/SpecificationRepository';
+import { SpecificationRepository } from '../modules/cars/repositories/implementations/SpecificationRepository';
 import { CreateSpacificationServer } from '../modules/cars/services/CreateSpecificationService';
 
 const specificationRoutes = Router();
