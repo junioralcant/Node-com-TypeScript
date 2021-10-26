2fa3f19ba2e01abef216b9621e299c9df5c3ca1d
Junior Marques <juniormarques@MacBook-Air-de-Junior.local>
2021-10-26 10:13:47 -0300

    Password encyption and user email validation

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { CreateUserController } from '../modules/accounts/useCases/CreateUserController';
+import { CreateUserController } from '../modules/accounts/useCase/CreateUserController';
 
 const userRoutes = Router();
 
