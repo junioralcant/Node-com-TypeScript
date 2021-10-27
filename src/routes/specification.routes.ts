42b59418f6b31aa47a4d12773433fe5701c7d9f4
Junior Marques <juniormarques@MacBook-Air-de-Junior.local>
2021-10-27 15:44:52 -0300

    Create authentication for routes

--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { CreateSpacificationController } from '../modules/cars/useCases/createSpecification/CreateSpecificationController';
 import { ListSpecificationController } from '../modules/cars/useCases/listSpecification/ListSpecificationController';
 
@@ -7,6 +8,8 @@ const specificationRoutes = Router();
 
 const createSpacificationController = new CreateSpacificationController();
 const listSpecificationController = new ListSpecificationController();
+
+specificationRoutes.use(ensureAuthenticated);
 
 specificationRoutes.post('/', createSpacificationController.handle);
 
